e245381193b5f74f2e0aaf74f945c86c4cace921
Vimeow <vy.nguyen.professional@gmail.com>
2024-01-22 20:53:52 +1030

    Create SQLite database

--- Vy_tempo/static/js/logic.js
+++ Vy_tempo/static/js/logic.js
@@ -9,7 +9,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-
+// Not used //
 // Highlight all the countries //
 // Use this link to get the GeoJSON data.
 // let link = "https://pkgstore.datahub.io/core/geo-countries/countries/archive/23f420f929e0e09c39d916b8aaa166fb/countries.geojson";
@@ -24,6 +24,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // // Highlight Australia only //
 // L.geoJson(country).addTo(myMap);
+// Not used//
 
 
 // Highlight filtered countries //
@@ -32,7 +33,35 @@ for (let i = 0; i < countries.length; i++) {
 }
 
 
+// Example of setting color condition//
+// Loop through the countries array, and create one marker for each country object.
+// for (let i = 0; i < countries.length; i++) {
 
+//   // Conditionals for country gdp_pc
+//   let color = "";
+//   if (countries[i].gdp_pc > 100000) {
+//     color = "yellow";
+//   }
+//   else if (countries[i].gdp_pc > 75000) {
+//     color = "blue";
+//   }
+//   else if (countries[i].gdp_pc > 50000) {
+//     color = "green";
+//   }
+//   else {
+//     color = "violet";
+//   }
+
+//   // Add circles to the map.
+//   L.circle(countries[i].location, {
+//     fillOpacity: 0.75,
+//     color: "white",
+//     fillColor: color,
+//     // Adjust the radius.
+//     radius: Math.sqrt(countries[i].gdp_pc) * 500
+//   }).bindPopup(`<h1>${countries[i].name}</h1> <hr> <h3>GDP Per Capita (USD): ${countries[i].gdp_pc}</h3>`).addTo(myMap);
+// }
+// Example of setting color condition//
 
 
 
