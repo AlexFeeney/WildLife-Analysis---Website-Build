5309fc1e18239b9f9873c7dd726f42cc7a6a2001
Vimeow <vy.nguyen.professional@gmail.com>
2024-01-26 22:36:27 +1030

    Edit output endpoint API to contain key:value pair format for easy data accessing later

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ from flask import (
     redirect)
 
 
-
-#todo this needs updateing
+# to do this needs updating
 engine = create_engine("sqlite:///Database/Conservation.db")
 
 # reflect an existing database into a new model
@@ -49,94 +48,242 @@ def data():
     return render_template("index.html")
 
 
-#--------------------Known Species Table------------------------
+# --------------------Known Species Table------------------------
+
+
+# @app.route("/api/knownSpecies")
+# def known_species():
+
+#     session = Session(engine)
+#     results = session.query(kp.Country, kp.Species,kp.Value).all()
+#     results = [list(r) for r in results]
+
+#     table_results = {
+#         "table": results
+#     }
+
+#     session.close()
+#     return jsonify(table_results)
+
 @app.route("/api/knownSpecies")
-def known_species():
+def knownSpecies():
 
+    # Create session (link) from Python to the DB
     session = Session(engine)
-    results = session.query(kp.Country, kp.Species,kp.Value).all()
-    results = [list(r) for r in results]
 
-    table_results = {
-        "table": results
-    }
+    # Query the data
+    results = session.query(kp.Country, kp.Species, kp.Value).all()
 
     session.close()
-    return jsonify(table_results)
 
+    # Create a dictionary
+    known_species = []
+    for row in results:
+        dt_dict = {}
+        dt_dict["country"] = row.Country
+        dt_dict["species"] = row.Species
+        dt_dict["value"] = row.Value
+        known_species.append(dt_dict)
+
+    return jsonify(known_species)
+
+
+# --------------------Critically endangered species Table------------------------
+
+
+# @app.route("/api/criticallyEndangeredSpecies")
+# def criticallyEndangeredSpecies():
+
+#     session = Session(engine)
+#     results = session.query(cep.Country, cep.Species, cep.Value).all()
+#     results = [list(r) for r in results]
+
+#     table_results = {
+#         "table": results
+#     }
+
+#     session.close()
+#     return jsonify(table_results)
 
 @app.route("/api/criticallyEndangeredSpecies")
 def criticallyEndangeredSpecies():
 
+    # Create session (link) from Python to the DB
     session = Session(engine)
-    results = session.query(cep.Country, cep.Species, cep.Value).all()
-    results = [list(r) for r in results]
 
-    table_results = {
-        "table": results
-    }
+    # Query the data
+    results = session.query(cep.Country, cep.Species, cep.Value).all()
 
     session.close()
-    return jsonify(table_results)
+
+    # Create a dictionary
+    critically_endangered_species = []
+    for row in results:
+        dt_dict = {}
+        dt_dict["country"] = row.Country
+        dt_dict["species"] = row.Species
+        dt_dict["value"] = row.Value
+        critically_endangered_species.append(dt_dict)
+
+    return jsonify(critically_endangered_species)
+
+# --------------------Endangered species Table------------------------
+
+
+# @app.route("/api/endangeredSpecies")
+# def endangeredSpecies():
+
+#     session = Session(engine)
+#     results = session.query(es.Country, es.Species, es.Value).all()
+#     results = [list(r) for r in results]
+
+#     table_results = {
+#         "table": results
+#     }
+
+#     session.close()
+#     return jsonify(table_results)
 
 @app.route("/api/endangeredSpecies")
 def endangeredSpecies():
 
+    # Create session (link) from Python to the DB
     session = Session(engine)
-    results = session.query(es.Country, es.Species, es.Value).all()
-    results = [list(r) for r in results]
 
-    table_results = {
-        "table": results
-    }
+    # Query the data
+    results = session.query(es.Country, es.Species, es.Value).all()
 
     session.close()
-    return jsonify(table_results)
+
+    # Create a dictionary
+    endangered_species = []
+    for row in results:
+        dt_dict = {}
+        dt_dict["country"] = row.Country
+        dt_dict["species"] = row.Species
+        dt_dict["value"] = row.Value
+        endangered_species.append(dt_dict)
+
+    return jsonify(endangered_species)
+
+
+# --------------------Threatened species Table------------------------
+
+
+# @app.route("/api/threatenedSpecies")
+# def threatenedSpecies():
+
+#     session = Session(engine)
+#     results = session.query(tp.Country, tp.Species, tp.Value).all()
+#     results = [list(r) for r in results]
+
+#     table_results = {
+#         "table": results
+#     }
+
+#     session.close()
+#     return jsonify(table_results)
 
 @app.route("/api/threatenedSpecies")
 def threatenedSpecies():
 
+    # Create session (link) from Python to the DB
     session = Session(engine)
-    results = session.query(tp.Country, tp.Species, tp.Value).all()
-    results = [list(r) for r in results]
 
-    table_results = {
-        "table": results
-    }
+    # Query the data
+    results = session.query(tp.Country, tp.Species, tp.Value).all()
 
     session.close()
-    return jsonify(table_results)
 
+    # Create a dictionary
+    threatened_species = []
+    for row in results:
+        dt_dict = {}
+        dt_dict["country"] = row.Country
+        dt_dict["species"] = row.Species
+        dt_dict["value"] = row.Value
+        threatened_species.append(dt_dict)
+
+    return jsonify(threatened_species)
+
+
+# --------------------Vulnerable species Table------------------------
+
+
+# @app.route("/api/vulenerableSpecies")
+# def vulenerableSpecies():
+
+#     session = Session(engine)
+#     results = session.query(vp.Country, vp.Species, vp.Value).all()
+#     results = [list(r) for r in results]
+
+#     table_results = {
+#         "table": results
+#     }
+
+#     session.close()
+#     return jsonify(table_results)
 
 @app.route("/api/vulenerableSpecies")
 def vulenerableSpecies():
 
+    # Create session (link) from Python to the DB
     session = Session(engine)
-    results = session.query(vp.Country, vp.Species, vp.Value).all()
-    results = [list(r) for r in results]
 
-    table_results = {
-        "table": results
-    }
+    # Query the data
+    results = session.query(vp.Country, vp.Species, vp.Value).all()
 
     session.close()
-    return jsonify(table_results)
+
+    # Create a dictionary
+    vulenerable_species = []
+    for row in results:
+        dt_dict = {}
+        dt_dict["country"] = row.Country
+        dt_dict["species"] = row.Species
+        dt_dict["value"] = row.Value
+        vulenerable_species.append(dt_dict)
+
+    return jsonify(vulenerable_species)
+
+
+# --------------------Countries Table------------------------
+
+
+# @app.route("/api/countries")
+# def countries():
+
+#     session = Session(engine)
+#     results = session.query(c.Country).all()
+#     results = [list(r) for r in results]
+
+#     table_results = {
+#         "table": results
+#     }
+
+#     session.close()
+#     return jsonify(table_results)
+
+
+# if __name__ == "__main__":
+#     app.run()
 
 @app.route("/api/countries")
 def countries():
 
+    # Create session (link) from Python to the DB
     session = Session(engine)
-    results = session.query(c.Country).all()
-    results = [list(r) for r in results]
 
-    table_results = {
-        "table": results
-    }
+    # Query the data
+    results = session.query(c.Country).all()
 
     session.close()
-    return jsonify(table_results)
 
+    # Create a dictionary
+    countries = []
+    for row in results:
+        dt_dict = {}
+        dt_dict["country"] = row.Country
+        countries.append(dt_dict)
 
-
-if __name__ == "__main__":
-    app.run()
+    return jsonify(countries)
